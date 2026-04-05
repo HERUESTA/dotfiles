259c9627957969ddd44b5aadc2fd23c0b64494de
HERUESTA <zgbh07@gmail.com>
2026-04-05 21:50:55 +0900

    nvimでマークダウンをプレビューできるプラグインを導入した

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      enabled = false,
+    },
+    keys = {
+      { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Markdown Render" },
+    },
+  },
+}
