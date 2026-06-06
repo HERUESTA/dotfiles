b0fd8cf83d252df12ef2ff7bdb20f5f5e922fe1c
HERUESTA <zgbh07@gmail.com>
2026-06-06 17:10:13 +0900

    マークダウンの改良

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "bullets-vim/bullets.vim",
+    ft = { "markdown", "text", "gitcommit" },
+    init = function()
+      vim.g.bullets_enabled_file_types = { "markdown", "text", "gitcommit" }
+      vim.g.bullets_outline_levels = { "ROM", "ABC", "num", "abc", "rom", "std-" }
+    end,
+  },
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       enabled = false,
@@ -18,3 +26,4 @@ return {
     },
   },
 }
+
