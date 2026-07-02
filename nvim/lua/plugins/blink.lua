bc1c3a2b3b9479104dd8f82ec5fee469b7536e8a
HERUESTA <zgbh07@gmail.com>
2026-07-02 22:47:19 +0900

    ci: 👷 入力候補をデフォルト

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -2,9 +2,7 @@ return {
   "saghen/blink.cmp",
   opts = {
     completion = {
-      menu = {
-        auto_show = false, --入力候補を勝手に出さないようにする
-      },
+      ghost_text = { enabled = false },
     },
   },
 }
