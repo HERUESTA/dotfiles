3abd6168dc6c4014c42009a17cb5212cbd294901
HERUESTA <zgbh07@gmail.com>
2026-07-02 22:52:15 +0900

    fix: 🐛 menuばーも無くし他

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -2,6 +2,9 @@ return {
   "saghen/blink.cmp",
   opts = {
     completion = {
+      menu = {
+        auto_show = false, --入力候補を勝手に出さないようにする
+      },
       ghost_text = { enabled = false },
     },
   },
